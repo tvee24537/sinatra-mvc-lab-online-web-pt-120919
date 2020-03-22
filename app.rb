ceb0a1a7af3b822782e7f0bc028ee5088d89f352
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-22 19:53:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do 
+
+    ring = PigLatinizer.new
+    @string = ring.pig(params[:user_phrase])  
+    erb :pig_latin
+
+  end 
 end
